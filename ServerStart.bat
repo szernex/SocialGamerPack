0d50185c846f6802dcb3cbca5559653b000645ce
Szernex <szernex@gmail.com>
2015-08-15 14:40:26 +0200

    script updates

--- ServerStart.bat
+++ ServerStart.bat
@@ -1,2 +1,10 @@
+if NOT EXIST minecraft_server.1.7.10.jar (
+    echo Missing required jars. Running install script!
+    call Install.bat
+)
+
+
+echo Starting server
 java -Dlog4j.configurationFile=log4j2.xml -Xms2048m -Xmx4096m -XX:PermSize=256m -XX:+UseG1GC -jar forge-1.7.10-10.13.4.1448-1.7.10-universal.jar nogui
+echo Server process finished
 pause
--- ServerStart.sh
+++ ServerStart.sh
@@ -1,2 +1,10 @@
+# run install script if MC server or launchwrapper s missing
+if [ ! -f minecraft_server.1.7.10.jar ]; then
+    echo "Missing required jars. Running install script!"
+    sh ./Install.sh
+fi
+
+echo "Starting server"
 java -Dlog4j.configurationFile=log4j2.xml -Xms2048m -Xmx4096m -XX:PermSize=256m -XX:+UseG1GC -jar forge-1.7.10-10.13.4.1448-1.7.10-universal.jar nogui
+echo "Server process finished"
 pause
