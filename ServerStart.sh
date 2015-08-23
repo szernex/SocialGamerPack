223cc5d1a0aa165424a007f9503cdb0974878962
Szernex <szernex@gmail.com>
2015-08-23 13:04:06 +0200

    * fixed linux scripts having wrong line endings

--- ServerStart.sh
+++ ServerStart.sh
@@ -7,4 +7,3 @@ fi
 echo "Starting server"
 java -Dlog4j.configurationFile=log4j2.xml -Xms2048m -Xmx4096m -XX:PermSize=256m -XX:+UseG1GC -jar forge-1.7.10-10.13.4.1448-1.7.10-universal.jar nogui
 echo "Server process finished"
-pause
